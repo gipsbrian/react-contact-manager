79be3aa61095c407c096dd9e29fdc88951decf59
wizGipsBrian <gipsbrian@gmail.com>
2019-01-24 14:52:13 +0300

    navbar bootstrap modification in the header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,26 +5,26 @@ import { Link } from "react-router-dom";
 const Header = props => {
   const { branding } = props;
   return (
-    <nav className="nav navbar navbar-expand-lg navbar-dark bg-danger mb-3 py-0">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <Link to="/" className="navbar-brand">
+        <a href="/" className="navbar-brand">
           {branding}
-        </Link>
+        </a>
         <div>
-          <ul className="navbar-bar mr-auto">
-            <li className="nav- item">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
               <Link to="/" className="nav-link">
-                <i className="fas fa-home">Home</i>
+                <i className="fas fa-home" /> Home
               </Link>
             </li>
-            <li className="nav- item">
+            <li className="nav-item">
               <Link to="/contact/add" className="nav-link">
-                <i className="fas fa-plus">Add</i>
+                <i className="fas fa-plus" /> Add
               </Link>
             </li>
-            <li className="nav- item">
+            <li className="nav-item">
               <Link to="/about" className="nav-link">
-                <i className="fas fa-question">About</i>
+                <i className="fas fa-question" /> About
               </Link>
             </li>
           </ul>
